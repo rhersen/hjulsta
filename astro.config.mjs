18891310687b295139a5aef7b3b511359a8815d9
rudolf.hersen@schibsted.se <rudolf.hersen@schibsted.com>
2025-01-14 15:09:39 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  output: "server",
+  adapter: netlify({}),
+  integrations: [preact()],
 });
